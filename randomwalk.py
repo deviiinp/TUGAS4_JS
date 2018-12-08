f602f6fb7e23d8642df5e06a648a491d9a1119c4
Devi Purnama Mulyono <37098639+deviiinp@users.noreply.github.com>
2018-12-08 20:58:40 +0700

    Update randomwalk.py

--- randomwalk.py
+++ randomwalk.py
@@ -1,31 +1,77 @@
 import networkx as nx 
-import matplotlib.pyplot as plt 
 import random
-import numpy
+import numpy as np
 
-#Function to calculate the number of random walks required to completely visit (visit each node at least once) a graph of n nodes.
-def walk(n,p):
-	start = random.randint(0,n-1)
-	G = nx.erdos_renyi_graph(n,p)
-	S = set([]) 
-	v = start
-	count = 0
-	while (len(S)<n):
-		Nbr = list(nx.neighbors(G,v))
-		v = random.choice(Nbr)
-		S.add(v)
-		count += 1
-	return count
+G=nx.DiGraph() 
 
-l = []
-for n in range(20,150): # Graph size vary from 20 nodes to 150 nodes
-	z = []
-	for j in range(10): # Taking 10 instances of random walks for a particular graph. Higher the no. of instances, more accurate is the no of walks required.
-		z.append(walk(n,0.3))
-	l.append(numpy.average(z)) # Taking the average of 10 instances of random walks on each graph.
-	print n, "---->", numpy.average(z)
-plt.plot(l)
-plt.xlabel('No of nodes in the graph', fontsize=12, color='green')
-plt.ylabel('No of random walks required for visiting all nodes', fontsize=12, color='green')
-plt.title('Random walks on a graph', fontsize=15, color='red')
-plt.show()
+G.add_edge('UU Nomor 22 Tahun 2001','UUD 1945') 
+G.add_edge('UU Nomor 22 Tahun 2001','Ketetapan MPR Nomor XV/MPR/1998')
+G.add_edge('UU Nomor 39 Tahun 2007','UUD 1945')
+G.add_edge('UU Nomor 39 Tahun 2007','UUD 1945')
+G.add_edge('UU Nomor 1 Tahun 2017','UU Nomor 24 Tahun 2000')
+G.add_edge('UU Nomor 18 Tahun 2017','UUD 1945') 
+G.add_edge('UU Nomor 18 Tahun 2017','UU Nomor 13 Tahun 2003')
+G.add_edge('UU Nomor 18 Tahun 2017','UU Nomor 6 Tahun 2012')
+G.add_edge('UU Nomor 2 Tahun 2018',' UUD 1945')
+G.add_edge('UU Nomor 2 Tahun 2018 ','UU Nomor 17 Tahun 2014') 
+   
+# Function to add edges to the directed graph
+def add_edges(G,p):
+	for i in G.nodes():
+		for j in G.nodes():
+			if i != j :
+				r = random.random()
+				if r <= p:
+					G.add_edge(i,j)
+				else:
+					continue
+	return G
+
+# Function to sort nodes according to their accumulated points
+# We will denote "random-walk-point" by "RWP"
+def get_nodes_sorted_by_RWP(RWP_list):
+	RWP_array = np.array(RWP_list) # Converting list to numpy array
+	nodes_sorted_by_RWP = np.argsort(-RWP_array) # The minus sign is used for sorting in descending order
+	return nodes_sorted_by_RWP
+
+# Function to perform random walks and increment random-walk-point of visited node by 1 
+def random_walk(G):
+	nodes = list(G.nodes()) # List of nodes of graph
+	RWP = [0 for i in range(G.number_of_nodes())]  # List containing RWP of all nodes which are initially 0
+	s_node = random.choice(nodes)  # Choosing starting (source) node at random
+	RWP[s_node] += 1
+	outlinked_neigh = G.out_edges(s_node)
+	c = 0 # Iteration variable
+	while (c != 100000):
+		if (len(outlinked_neigh) == 0): # Case when the node has no outlink
+			focus = random.choice(nodes) # focus is the node currently being visited
+		else: 
+			random_neigh = random.choice(list(outlinked_neigh)) 
+			focus = random_neigh[1]
+		RWP[focus] += 1
+		outlinked_neigh = G.out_edges(focus)
+		c += 1
+	return RWP
+
+# Function for main program	
+def main():
+	G = nx.DiGraph() # Creating a directed graph object
+	N = 10 # Number of nodes in the graph
+	p = 0.3 # Probability for generating random graph
+	G.add_nodes_from([i for i in range(N)]) # Adding nodes to the graph object
+	G = add_edges(G,p) # Adding edges to the graph object, making it a random graph
+
+	RWP_list = random_walk(G) # Performing random walks on the graph and getting list of RWP for nodes
+	nodes_sorted_by_RWP = get_nodes_sorted_by_RWP(RWP_list) # Ranking (ordering) nodes on the basis of accumulated RWP
+
+	print 'Nodes sorted (ranked) according to the accumulated random-walk-points : ', 
+	for i in nodes_sorted_by_RWP:
+		print i,
+
+	pr = nx.pagerank(G) # Getting PageRank of nodes, output is a dictionary
+	pr_sorted = sorted(pr.items(), key=lambda x:x[1], reverse=True) 
+	print '\nNodes sorted (ranked) using the builtin PageRank algo function : ',
+	for i in pr_sorted:
+		print i[0],
+
+main() # Invoking the main function
